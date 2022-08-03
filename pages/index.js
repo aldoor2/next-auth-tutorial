64eb0d1b39fefc3dee56bc1becf9a4aa02633cdd
Alphonse <apuertaj2@gmail.com>
2022-08-02 19:59:55 -0500

    Implementing NextAuth functions

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
-import { getSession } from 'next-auth/react'
+import { getSession, signOut } from 'next-auth/react'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
 
 function HomePage({ session }) {
-
-  const router = useRouter()
-
   return (
     <div>
       {
@@ -20,11 +16,14 @@ function HomePage({ session }) {
                 width='100%'
                 height='100%'
               />
-              <button onClick={() => router.push('/api/auth/signout')}>Logout</button>
             </>
           )
           : (<p>Skeleton</p>)
       }
+
+      <button onClick={() => signOut()}>
+        Logout
+      </button>
     </div>
   )
 }
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,20 @@
+import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 
 function LoginPage() {
+
+  const { data: session, status } = useSession()
   const router = useRouter()
+
+  if (status !== 'loading' && status === 'authenticated') {
+    router.push('/')
+    return
+  }
+
   return (
-    <button onClick={() => router.push('/api/auth/signin/github')}>Signin with Github</button>
+    <button onClick={() => signIn('github')}>
+      Signin with Github
+    </button>
   )
 }
 
